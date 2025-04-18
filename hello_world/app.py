53ab9f2ae1cdd94eac513127cc6bf427ec451d8d
Spran02 <praneethsunku@gmail.com>
2025-04-18 05:30:02 +0530

    some change

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world again 7.0",
+            "message": "hello world again 8.0",
             # "location": ip.text.replace("\n", "")
         }),
     }
